524b160f40642c0851e98a034970ba78f0dc6a7c
Ralph Haddad <ralphhaddad777@gmail.com>
2025-05-28 08:35:23 -0700

    Quartz sync: May 28, 2025, 8:35 AM

---
+++ content/to raine.md
@@ -0,0 +1,6 @@
+---
+draft: "false"
+tags:
+  - tofriends
+---
+Hi Raine, you found my little note to you. Thank you for supporting me. I love you.
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "Ralph Haddad",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "ralphhaddad.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
