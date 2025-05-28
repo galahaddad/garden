cd0fbacd5ab8100fedaf92c5bb806d45db04dd21
Ralph Haddad <ralphhaddad777@gmail.com>
2025-05-28 08:01:55 -0700

    Quartz sync: May 28, 2025, 8:01 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,9 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/galahaddad",
+      LinkedIn: "https://www.linkedin.com/in/haddad-ralph/"
+      //"Discord Community": "https://discord.gg/cRFFHYye7t",
     },
   }),
 }
